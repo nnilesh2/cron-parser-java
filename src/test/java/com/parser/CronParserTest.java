b9d1ed91d7bf2f78c4561c5092e55f7f06040757
Nilesh Nagare <37430498+nnilesh2@users.noreply.github.com>
2024-11-26 19:19:28 +0000

    Improved test cases

--- src/test/java/com/parser/CronParserTest.java
+++ src/test/java/com/parser/CronParserTest.java
@@ -23,48 +23,55 @@ public class CronParserTest {
 
         String[] args = {};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"No cron expression provided");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("No cron expression provided"));
     }
 
     @Test
     public void test_cron_validator_minute_failure() {
         String[] args = {"*/61 0 1,15 * 1-5 /usr/bin/find"};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Minute value should be withing range of 0-59");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Minute value should be withing range of 0-59"));
     }
 
     @Test
     public void test_cron_validator_hour_failure() {
         String[] args = {"*/15 25 1,15 * 1-5 /usr/bin/find"};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Hour value should be withing range of 0-23");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Hour value should be withing range of 0-23"));
     }
 
     @Test
     public void test_cron_validator_day_failure() {
         String[] args = {"*/15 0 32,15 * 1-5 /usr/bin/find"};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Day value should be withing range of 1-31");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Day value should be withing range of 1-31"));
     }
 
     @Test
     public void test_cron_validator_month_failure() {
         String[] args = {"*/15 0 1,15 13 1-5 /usr/bin/find"};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Monht value should be withing range of 1-12");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Monht value should be withing range of 1-12"));
     }
 
     @Test
     public void test_cron_validator_week_failure() {
         String[] args = {"*/15 0 1,15 * 1-8 /usr/bin/find"};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Week value should be withing range of 0-7");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Week value should be withing range of 0-7"));
     }
 
     @Test
     public void test_cron_validator_command_failure() {
         String[] args = {"*/15 0 1,15 * 1-5  "};
         CronExpression cronExpression = new CronExpression(args);
-        assertThrows(IllegalArgumentException.class, ()->cronExpression.validate(),"Cron command can not be null or blank");
+        var exception = assertThrows(IllegalArgumentException.class, ()-> cronExpression.validate());
+        assertTrue(exception.getMessage().equals("Cron command can not be null or blank"));
     }
 }
